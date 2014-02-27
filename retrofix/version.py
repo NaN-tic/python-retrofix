b86e656ba9ca09dd9598194375e2c230d798b75a
?ngel ?lvarez Serra <angel@nan-tic.com>
2014-02-27 09:47:01 +0100

    increase version

--- retrofix/version.py
+++ retrofix/version.py
@@ -19,6 +19,6 @@
 ##############################################################################
 
 PACKAGE = 'retrofix'
-VERSION = '0.5'
+VERSION = '0.6'
 LICENSE = 'GPL-3'
 WEBSITE = 'http://www.NaN-tic.com/'
