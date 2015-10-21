08b53b5e9cc2471d3e546d7220ed086581c4643b
Albert Cervera i Areny <albert@nan-tic.com>
2015-10-21 12:47:01 +0200

    Increase version 0.12.

--- retrofix/version.py
+++ retrofix/version.py
@@ -19,6 +19,6 @@
 ##############################################################################
 
 PACKAGE = 'retrofix'
-VERSION = '0.11'
+VERSION = '0.12'
 LICENSE = 'GPL-3'
 WEBSITE = 'http://www.NaN-tic.com/'
