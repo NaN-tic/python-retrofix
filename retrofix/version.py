89640e1228e15f590139d9a18904aa201da8d1de
Guillem Barba <guillem@nan-tic.com>
2015-12-10 13:42:44 +0100

    New version 0.14

--- retrofix/version.py
+++ retrofix/version.py
@@ -19,6 +19,6 @@
 ##############################################################################
 
 PACKAGE = 'retrofix'
-VERSION = '0.13'
+VERSION = '0.14'
 LICENSE = 'GPL-3'
 WEBSITE = 'http://www.NaN-tic.com/'
