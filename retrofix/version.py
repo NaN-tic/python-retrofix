9cdffbd0025e47945334acbdb49445e40167f162
Guillem Barba <guillem@nan-tic.com>
2016-03-17 10:38:07 +0100

    Prepare release 0.19

--- retrofix/version.py
+++ retrofix/version.py
@@ -19,6 +19,6 @@
 ##############################################################################
 
 PACKAGE = 'retrofix'
-VERSION = '0.18'
+VERSION = '0.19'
 LICENSE = 'GPL-3'
 WEBSITE = 'http://www.NaN-tic.com/'
