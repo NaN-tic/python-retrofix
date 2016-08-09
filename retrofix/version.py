2e950053f6b139490822b53a456646b8a8bff58e
Albert Cervera i Areny <albert@nan-tic.com>
2016-08-09 16:01:28 +0200

    Prepare for 0.21 release.

--- retrofix/version.py
+++ retrofix/version.py
@@ -19,6 +19,6 @@
 ##############################################################################
 
 PACKAGE = 'retrofix'
-VERSION = '0.20'
+VERSION = '0.21'
 LICENSE = 'GPL-3'
 WEBSITE = 'http://www.NaN-tic.com/'
