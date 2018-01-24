dbe69cad03ed0332fe7ba0e15b942be263e4a008
Raimon Esteve <raimon@nan-tic.com>
2018-01-24 16:28:11 +0100

    Prepare 0.24.1 release

--- retrofix/version.py
+++ retrofix/version.py
@@ -19,6 +19,6 @@
 ##############################################################################
 
 PACKAGE = 'retrofix'
-VERSION = '0.24'
+VERSION = '0.24.1'
 LICENSE = 'GPL-3'
 WEBSITE = 'http://www.NaN-tic.com/'
