76513171032a6b22a5d02b1863884307cd7ec055
Bernat Brunet <bernat@nan-tic.com>
2023-04-13 12:11:08 +0200

    Update version

--- retrofix/version.py
+++ retrofix/version.py
@@ -19,6 +19,6 @@
 ##############################################################################
 
 PACKAGE = 'retrofix'
-VERSION = '0.29.2'
+VERSION = '0.29.3'
 LICENSE = 'GPL-3'
 WEBSITE = 'http://www.NaN-tic.com/'
